e228798667de541d9852487c69fc2504d13b687e
Ding_D <1601202253@qq.com>
2025-05-16 16:24:50 +0800

    perf: 最小宽度

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
           },
         }}
       >
-        <div className="overflow-hidden sm:pt-[46px] ssm:pt-[91px] sm:w-[1920px] sm:mx-auto ssm:w-full">
+        <div className="overflow-hidden sm:pt-[46px] ssm:pt-[91px] sm:min-w-[1400px] sm:mx-auto ssm:w-full">
           <AiConsult />
           <Nav />
           <Header />
